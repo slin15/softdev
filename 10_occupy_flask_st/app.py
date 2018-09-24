59680ab9e4acdf2a7c4f84398df2ade1da52f526
Susan Lin <slin15@stuy.edu>
2018-09-23 22:29:33 -0400

    stuff

--- 10_occupy_flask_st/app.py
+++ 10_occupy_flask_st/app.py
@@ -10,28 +10,36 @@ import random
 diction = {}
 
 def convertToDict(filename):
-    f = open(filename, 'r')
-    text = f.read().split("\n")
-    print(text)
+    text = open(filename, 'r').read().split("\n")
+    #turns the csv file into a list separated by commas
     for x in range (1, len(text)- 2):
         cat = text[x].rsplit(',', 1)
+        #splits each string in text into title and percent
         title = cat[0].strip('"')
+        #removes quotes from title 
         percent = float(cat[-1])
+        #typecasts the percent into a float 
         diction[title] = percent
+        #sets the value of the title key in diction to percent 
 
 def findLast():
     diff = 100.0
     for key in diction:
         diff -= diction[key]
-    return ("%.1f" % diff) 
+        #subtracts percent of each occupation from total 
+    return ("%.1f" % diff)
+    #rounds the difference to the nearest tenth 
 
 def randomOcc():
     randlist = []
     for key in diction:
         current = diction[key]
+        #stores the percent of each title
         freq = int(current *10)
+        #we are storing each title by percent (x10 to rid decimal) in a list 
         for i in range (freq):
             randlist.append(key)
+        #returns a randomly selected occupation
     return (random.choice(randlist))
        
 @app.route('/')
