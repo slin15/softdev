e849e169ab071dfb469dbf9b394cc5682e982498
robinhanstuy <rhan@stuy.edu>
2018-10-01 23:40:46 -0400

    comments added

--- 14_login/app.py
+++ 14_login/app.py
@@ -9,23 +9,29 @@ from os import urandom
 app = Flask(__name__)
 
 app.secret_key = urandom(32)
+#hard coded valid username and 
 users = {"slin15": "12345"} 
        
+#home root
 @app.route('/', methods=["POST", "GET"])
 def home():
      return render_template('form.html')
-
+#reading in user and password, checking to see if it is valid or not
 @app.route('/login', methods=["POST", "GET"])
 def login():    
     if request.args['user'] in users.keys() and users[request.args['user']] == request.args['pass']:
-         return render_template('return.html', user=request.args['user'], password=request.args['pass']) 
-    
+	#username and passwords match         
+	return render_template('return.html', user=request.args['user'], password=request.args['pass']) 
+	    
     elif request.args['user'] not in users.keys():
-        return render_template('error.html', error="username not found")
+        #username doesn't match
+	return render_template('error.html', error="username not found")
 
     elif users[request.args['user']] != request.args['pass']:
-        return render_template('error.html', error="incorrect password")
+	#password doesn't match username        
+	return render_template('error.html', error="incorrect password")
 
+# logout route, sends user back to home root and forgets current user
 @app.route("/logout", methods=["POST", "GET"])
 def logout():
     session.pop['slin15'] 
