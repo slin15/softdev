8d9dcd6f9d4420eef386a706259122cfd36eaf5a
Susan Lin <slin15@stuy.edu>
2018-10-01 20:53:37 -0400

    s

---
+++ 14_login/app.py
@@ -0,0 +1,26 @@
+# Team : Robin Han and Susan Lin 
+# SoftDev pd8
+# K #14: Do I Know You?    this
+# 2018-10-01
+
+from flask import Flask, render_template, request, session, redirect, url_for
+app = Flask(__name__)
+       
+@app.route('/')
+
+def disp_login():
+    return render_template('form.html') 
+
+@app.route('/auth')
+def authenticate():
+    print(url_for('disp_login'))
+    print(url_for('authenticate'))
+    return(redirect(url_for('disp_login')))
+    #return render_template('return.html',
+    #                       user=request.args['in'],password = request.args['pa$
+    #                      method = request.method)
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
