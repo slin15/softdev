94bfbf61fa1fabf820c62eb3a5eab2cdd01ec198
Susan Lin <slin15@stuy.edu>
2018-10-01 21:01:20 -0400

    added

--- 14_login/app.py
+++ 14_login/app.py
@@ -4,18 +4,21 @@
 # 2018-10-01
 
 from flask import Flask, render_template, request, session, redirect, url_for
-app = Flask(__name__)
-       
-@app.route('/')
+from os import urandom
 
+app = Flask(__name__)
+
+app.secret_key = urandom(32) 
+       
+@app.route('/', methods=["GET"])
 def disp_login():
     return render_template('form.html') 
 
 @app.route('/auth')
 def authenticate():
-    print(url_for('disp_login'))
-    print(url_for('authenticate'))
-    return(redirect(url_for('disp_login')))
+    print (url_for('disp_login'))
+    print (url_for('authenticate'))
+    return (redirect(url_for('disp_login')))
     #return render_template('return.html',
     #                       user=request.args['in'],password = request.args['pa$
     #                      method = request.method)
