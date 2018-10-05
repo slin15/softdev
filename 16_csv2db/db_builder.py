b7a5252ff8e0321cf4a1028da2a7e3efcfcc7915
Susan Lin <slin15@stuy.edu>
2018-10-04 20:19:46 -0400

    s

---
+++ 16_csv2db/db_builder.py
@@ -0,0 +1,42 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev1 pd0
+#SQLITE3 BASICS
+#2018-10-04
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+
+
+print("STUDENTS")
+with open('peeps.csv') as csvfile:
+    #command = "CREATE TABLE roster (name TEXT, age INTEGER, userid INTEGER)"        #build SQL stmt, save as string
+    #c.execute(command)    #run SQL statement 
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        #command = "INSERT INTO roster VALUES(row['name'])"
+        #c.execute(command)
+        
+
+print("\n COURSES")
+with open('courses.csv') as csvfile:
+    command = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"        #build SQL stmt, save as string
+    c.execute(command)    #run SQL statement
+    reader = csv.DictReader(csvfile)
+    for row in reader: 
+        print(row['code'], row['mark'], row['id'])
+
+#==========================================================
+
+
+db.commit() #save changes
+db.close()  #close database
+
+
