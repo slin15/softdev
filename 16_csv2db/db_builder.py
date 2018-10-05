8839e8f5c69187e09e83c2bb2dd476dd980798c2
Susan Lin <slin15@stuy.edu>
2018-10-04 21:47:55 -0400

    s

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,4 +1,4 @@
-#Clyde "Thluffy" Sinclair
+#Team hElPL : Cheryl Qian, Susan Lin, Simon Tsui 
 #SoftDev1 pd0
 #SQLITE3 BASICS
 #2018-10-04
@@ -14,27 +14,25 @@ c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
-print("STUDENTS")
 with open('peeps.csv') as csvfile:
-    #command = "CREATE TABLE roster (name TEXT, age INTEGER, userid INTEGER);"        #build SQL stmt, save as string
-    #c.execute(command)    #run SQL statement 
+    command = "CREATE TABLE roster (name TEXT, age INTEGER, id INTEGER PRIMARY KEY)"        #build SQL stmt, save as string
+    c.execute(command)    #run SQL statement
+    
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row['name'], row['age'], row['id'])
-        #command = "INSERT INTO roster VALUES(row['name'])"
-        #c.execute(command)
+        c.execute("INSERT INTO roster (name, age, id) VALUES('" +row['name']+"','"+row['age']+"','"+row['id']+"');")
         
+#c.execute("SELECT * FROM roster;")
+#print( c.fetchall())
 
-print("\n COURSES")
 with open('courses.csv') as csvfile:
-    #command = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);"        #build SQL stmt, save as string
-    #c.execute(command)    #run SQL statement
+    command = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);"        #build SQL stmt, save as string
+    c.execute(command)    #run SQL statement
     reader = csv.DictReader(csvfile)
-    for row in reader: 
-        print(row['code'], row['mark'], row['id'])
+    for row in reader:
+           c.execute("INSERT INTO courses (code, mark, id) VALUES('" +row['code']+"','"+row['mark']+"','"+row['id']+"');")
 
 #==========================================================
-
 
 db.commit() #save changes
 db.close()  #close database
